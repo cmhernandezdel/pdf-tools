eeddfe23346295b82f520a398636c65ac9ff2cfc
cmhernandezdel <cmhernandezdel@gmail.com>
2019-08-18 22:07:59 +0200

    Started to create the GUI

---
+++ gui/gui.py
@@ -0,0 +1,28 @@
+from tkinter import *
+
+# Definitions
+def present_layout(option):
+  return 
+
+# Variables
+window = Tk()
+menu_bar = Menu(window)
+
+# Function select menu
+functions_menu = Menu(menu_bar, tearoff=0)
+functions_menu.add_command(label="Merge PDFs", command=present_layout)
+functions_menu.add_command(label="Split PDF", command=present_layout)
+functions_menu.add_command(label="Delete pages", command=present_layout)
+functions_menu.add_command(label="Extract part of a PDF", command=present_layout)
+
+# Add menus to the menubar
+menu_bar.add_cascade(label="Functions", menu=functions_menu)
+
+# Add menu bar to window
+window.config(menu=menu_bar)
+
+# Set window title
+window.title("PDF tools")
+
+# Init window
+window.mainloop()
